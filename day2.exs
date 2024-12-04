a629e9bab43ec3112a09fd2d19e5675d613f3ad3
pepe217 <michael.r.pepe@gmail.com>
2024-12-03 19:45:54 -0800

    Day 2

---
+++ day2.exs
@@ -0,0 +1,49 @@
+defmodule Day2 do
+  def part1 do
+    File.read!("day2.txt")
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(fn row -> String.split(row) |> Enum.map(&String.to_integer/1) end)
+    |> Enum.map(fn row -> difference(row, []) end)
+    |> Enum.map(&is_safe/1)
+    |> Enum.count(fn item -> item end)
+  end
+
+  def part2 do
+    File.read!("day2.txt")
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(fn row -> String.split(row) |> Enum.map(&String.to_integer/1) end)
+    |> Enum.map(fn row ->
+      Enum.any?(permutations(row), fn l -> is_safe(difference(l, [])) end)
+    end)
+    |> Enum.count(fn item -> item end)
+  end
+
+  def is_safe(row) do
+    Enum.all?(row, fn item -> item >= 1 && item <= 3 end) ||
+      Enum.all?(row, fn item -> item >= -3 && item <= -1 end)
+  end
+
+  def permutations(list) do
+    for n <- 0..length(list) do
+      List.delete_at(list, n)
+    end
+  end
+
+  def difference(list, acc) do
+    case list do
+      [first, second | tail] when tail != [] ->
+        difference([second] ++ tail, acc ++ [first - second])
+
+      [first, second | _tail] ->
+        acc ++ [first - second]
+
+      [_first] ->
+        acc
+
+      [] ->
+        acc
+    end
+  end
+end
